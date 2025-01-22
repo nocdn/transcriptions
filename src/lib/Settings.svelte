9cff976352b4c7de60f53d0da9a4507e73d3f156
Bartek Bak <bartek.bak@protonmail.com>
2025-01-22 15:11:35 +0000

    fix: currentModelProvider not changing in localstorage

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -5,7 +5,7 @@
   import { Zap } from "lucide-svelte";
   import { Languages } from "lucide-svelte";
 
-  let { close, settings } = $props();
+  let { close } = $props();
 
   let advancedChecked = $state(false);
   let selectedFormat = $state("1");
@@ -23,11 +23,29 @@
   let fireworksPromptValue = $state("");
   let fireworksLanguageValue = $state("en");
 
+  let settings = $state({
+    currentModelProvider: "groq",
+    groq: {
+      groqModelValue: "whisper-large-v3",
+      groqPromptValue: "",
+      groqLanguageValue: "en",
+    },
+    gemini: {
+      geminiModelValue: "gemini-2.0-flash-exp",
+      geminiPromptValue: "",
+    },
+    fireworks: {
+      fireworksModelValue: "accounts/fireworks/models/whisper-v3-turbo",
+      fireworksPromptValue: "",
+      fireworksLanguageValue: "en",
+    },
+  });
+
   // Load settings from localStorage on mount
   onMount(() => {
     const savedSettings = localStorage.getItem("transcription-settings");
     if (savedSettings) {
-      const settings = JSON.parse(savedSettings);
+      settings = JSON.parse(savedSettings);
       currentModelProvider = settings.currentModelProvider;
       if (currentModelProvider === "groq") {
         generalModelValue = settings.groq.groqModelValue;
@@ -60,37 +78,42 @@
     close();
   }
 
-  let generalModelValue = $state("whisper-large-v3");
+  let currentModelProvider = $state("groq");
+  let generalModelValue = $state("");
   function handleModelChange() {
     if (generalModelValue.includes("gemini")) {
-      currentModelProvider = "gemini";
+      settings.currentModelProvider = "gemini";
+      settings.gemini.geminiModelValue = generalModelValue;
+      console.log("currentModelProvider", currentModelProvider);
     } else if (generalModelValue.includes("fireworks")) {
-      currentModelProvider = "fireworks";
+      settings.currentModelProvider = "fireworks";
+      settings.fireworks.fireworksModelValue = generalModelValue;
+      console.log("currentModelProvider", currentModelProvider);
     } else {
-      currentModelProvider = "groq";
+      settings.currentModelProvider = "groq";
+      settings.groq.groqModelValue = generalModelValue;
+      console.log("currentModelProvider", currentModelProvider);
     }
   }
 
-  let currentModelProvider = $state("groq");
-
   function handleSave() {
-    const settings = {
-      currentModelProvider,
-      groq: {
-        groqModelValue,
-        groqPromptValue,
-        groqLanguageValue,
-      },
-      gemini: {
-        geminiModelValue,
-        geminiPromptValue,
-      },
-      fireworks: {
-        fireworksModelValue,
-        fireworksPromptValue,
-        fireworksLanguageValue,
-      },
-    };
+    // const settings = {
+    // currentModelProvider,
+    // groq: {
+    //   groqModelValue,
+    //   groqPromptValue,
+    //   groqLanguageValue,
+    // },
+    // gemini: {
+    //   geminiModelValue,
+    //   geminiPromptValue,
+    // },
+    // fireworks: {
+    //   fireworksModelValue,
+    //   fireworksPromptValue,
+    //   fireworksLanguageValue,
+    // },
+    // };
 
     // Save to localStorage
     localStorage.setItem("transcription-settings", JSON.stringify(settings));
@@ -308,7 +331,7 @@
           </tbody>
         </table>
       </div>
-      {#if currentModelProvider === "groq"}
+      {#if settings.currentModelProvider === "groq"}
         <div class="space-y-2" style="--ring: 234 89% 74%;">
           <label
             class="text-sm font-medium leading-4 text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -340,7 +363,7 @@
             </div>
           </div>
         </div>
-      {:else if currentModelProvider === "gemini"}
+      {:else if settings.currentModelProvider === "gemini"}
         <!-- Gemini -->
         <div class="space-y-2" style="--ring: 234 89% 74%;">
           <label
@@ -353,7 +376,7 @@
             bind:value={geminiPromptValue}
           ></textarea>
         </div>
-      {:else if currentModelProvider === "fireworks"}
+      {:else if settings.currentModelProvider === "fireworks"}
         <!-- Fireworks -->
         <div class="space-y-2" style="--ring: 234 89% 74%;">
           <label
@@ -533,20 +556,6 @@
           class="text-sm font-medium flex gap-3 items-center border border-input dark:border-gray-500 rounded-md px-4 py-2 hover:bg-[#F6EAEA] hover:text-red-800 hover:outline-1 hover:outline-dashed hover:outline-[#b8040487]"
           onmousedown={handleCancel}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-4"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-            />
-          </svg>
           Cancel
         </button>
         <button
